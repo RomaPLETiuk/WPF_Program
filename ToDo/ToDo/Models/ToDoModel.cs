f98f00f0e6eff799bf8330df5dbd1044e9966bb9
RomaPLETiuk <108412429+RomaPLETiuk@users.noreply.github.com>
2023-06-25 21:12:23 +0300

    Add files via upload

---
+++ ToDo/ToDo/Models/ToDoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ToDo.Models
+{
+    public class ToDoModel : INotifyPropertyChanged
+    {
+        public DateTime Date { get; set; } = DateTime.Now;
+
+
+        private bool _isDone;
+
+        public bool IsDone 
+        { 
+            get { return _isDone; } 
+            set 
+            { if (_isDone == value) return;
+                _isDone = value; 
+                OnPropertyChanged("IsDone");
+            } 
+        }
+
+
+        private string _task;
+
+        public string Task
+        {
+            get { return _task; }
+            set 
+            { 
+                if (_task == value) return;
+                _task = value; 
+                OnPropertyChanged("Task");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+            //PropertyChanged (this, new PropertyChangedEventArgs (propertyName));
+        }
+    }
+}
---
+++ ToDo/ToDo/Services/FileIOService.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Models;
+
+namespace ToDo.Services
+{
+    
+    public class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+
+        public BindingList<ToDoModel> LoadData() 
+        {
+            var fileExist = File.Exists(PATH);
+            if (!fileExist) 
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+            using(var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();  
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+
+            }
+            
+        }
+
+        public BindingList<ToDoModel> SaveData(object toDoDateList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(toDoDateList);
+                writer.WriteLine(output);
+
+
+            }
+
+            return new BindingList<ToDoModel>();
+
+        }
+    }
+}
